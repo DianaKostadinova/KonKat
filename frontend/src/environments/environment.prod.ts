f6b4351b3673fff00b22b15952be555afafdf7b0
DianaKostadinova <dkostadinova112@gmail.com>
2026-06-01 01:38:53 +0200

    track angular environment files for vercel build

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  apiUrl: '/api',
+  wsUrl:  'wss://konkat-api.onrender.com/ws',
+ firebase: {
+   apiKey:    'AIzaSyA7m0cCDDjP6nM7NDZTPmXLv2SREUDDywM',
+   authDomain: 'k0nkat.firebaseapp.com',
+   projectId: 'k0nkat',
+ },
+};
